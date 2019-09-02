d751b0ddecd948c2ac27a093e6bfa1dfc3047e99
Andras Petres <andras.petres@itiviti.com>
2019-09-02 16:19:06 +0300

    Stop TestComplete gracefullt
    Change-Id: Ide77ad74add0e6249028c857313e4dfb55fd30ba

---
+++ StopTestComplete/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Threading;
+
+namespace StopTestComplete
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            string[] progIDs = new string[] {
+                "TestComplete.TestCompleteApplication",
+                "TestComplete.TestCompleteApplication.12",
+                "TestComplete.TestCompleteX64Application",
+                "TestComplete.TestCompleteX64Application.12",
+                "TestComplete.TestExecuteApplication",
+                "TestComplete.TestExecuteX64Application",
+                "TestExecute.TestExecuteApplication",
+                "TestExecute.TestExecuteApplication.12",
+                "TestExecute.TestExecuteX64Application",
+                "TestExecute.TestExecuteX64Application.12"
+            };
+
+            string tcProgID = null;
+            object testCompleteObject = null;
+
+            foreach(string progId in progIDs)
+            {
+                try
+                {
+                    testCompleteObject = Marshal.GetActiveObject(progId);
+                    tcProgID = progId;
+                    break;
+                }
+                catch
+                {
+                    //do nothing and try the next one
+                }
+            }
+
+            if (testCompleteObject == null)
+            {
+                Console.WriteLine("Failed to connect to TestComplete/TestExecute.");
+                return 2;
+            }
+            Console.WriteLine("Successfully connected to " + tcProgID);
+
+            // Obtains ITestCompleteCOMManager
+            TestComplete.ITestCompleteCOMManager testCompleteManager = (TestComplete.ITestCompleteCOMManager)testCompleteObject;
+            // Obtains Integration object
+            TestComplete.ItcIntegration integrationObject = testCompleteManager.Integration;
+
+            if (integrationObject.IsRunning())
+            {
+                Console.WriteLine("TestComplete is executing tests. Stopping it ...");
+                stopTcAndWait(integrationObject);
+                Console.WriteLine("TestComplete was stopped.");
+            }
+            else
+            {
+                Console.WriteLine("TestComplete is not running any tests.");
+                return 1;
+            }
+            Marshal.ReleaseComObject(testCompleteObject);
+            return 0;
+        }
+
+        private static void stopTcAndWait(TestComplete.ItcIntegration integrationObject)
+        {
+            integrationObject.Stop();
+            try
+            {
+                while (integrationObject.IsRunning())
+                {
+                    Console.WriteLine("Waiting for TC to stop ...");
+                    Thread.Sleep(2000);
+                }
+            }
+            catch { }
+        }
+    }
+}
